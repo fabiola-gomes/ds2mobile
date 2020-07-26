db7523070a7bd1813e8b684b039f2b44c7e8305a
Gustavo de Souza Lemos <32879380+gxrj@users.noreply.github.com>
2020-07-26 02:12:36 -0300

    Update inicio.page.ts

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,35 +1,15 @@
-import { Component, OnInit } from '@angular/core'
-
-import {LoginService} from './login.service'
-import { Aluno } from '../interfaces/aluno'
-import { Router } from '@angular/router'
-import { isUndefined } from 'util'
-import { Sessao } from '../sessao/sessao'
-import { Observable } from 'rxjs'
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class InicioPage implements OnInit {
 
-    login: string
-    senha: string
-  
-    aluno$: Observable<Aluno>
-
-  constructor(private loginService: LoginService, private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  autenticar(){
-
-    this.aluno$ = this.loginService.getAluno(this.login, this.senha)
-    this.aluno$.subscribe( dados => Sessao.usuario  = dados,
-                            err => {/*Fazer pop-out de erro */},
-                            () => this.router.navigate(['/estagio']))                        
-  }
-  
 }
